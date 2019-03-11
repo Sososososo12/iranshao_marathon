536232a31db7ef07494dd05821fb971d155c7147
Sosososj <youamg2@126.com>
2019-03-11 15:11:08 +0800

    test pandas operation in loading&cut

--- comment/sentiments.py
+++ comment/sentiments.py
@@ -1,15 +1,25 @@
 from snownlp import SnowNLP
 from comment import cut_sentence
+import pandas as pd
+import xlrd
+
+data = xlrd.open_workbook(filename=r'./cut_sentence.xls')
+sheet1 = data.sheet_by_index(0)
+comment_input = sheet1.col_values(0)
+# people_id = sheet1.col_values(1)
+
 
 output_sentence=[]
 input_sentence=[]
-inputs=cut_sentence.cutSentence()
-for i in inputs:
-    i.replace(' ','')
-    if i!='':
-        input_sentence.append(i)
 
-for line in inputs:
+
+for line in comment_input:
     a=SnowNLP(line)
     output_sentence.append(a.sentiments)
-print(output_sentence)
+# print(output_sentence)
+data1 = pd.DataFrame({'sentiments':output_sentence,
+
+                      })
+data1.to_excel(u'comment_sentiment.xls', index=False, encoding='"utf_8_sig')
+print('信息写入完成！')
+
