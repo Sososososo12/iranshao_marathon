38546a2f120a026010f314c78b3b0558cb425434
Sosososj <youamg2@126.com>
2019-05-02 01:33:38 +0800

    finish all takepart py prepare for test

--- event_recommand/te2.py
+++ event_recommand/te2.py
@@ -4,9 +4,7 @@ import os
 import pandas as pd
 import time
 from event_recommand import get_user_partinrace
-from event_recommand import email attention
-
-
+from event_recommand import email_attention
 
 # userlenfilename = r'./event_participant/{}usertest2.xls'.format(str(583))
 # user_Fdata = xlrd.open_workbook(filename=userlenfilename)
@@ -32,19 +30,9 @@ data = xlrd.open_workbook(filename=dataname)
 sheet1 = data.sheet_by_index(1)
 user_id_set = sheet1.col_values(0)
 user_id_len = len(user_id_set)
-# print(user_id_len)
-startnum=15000
-stopnum=startnum+5000
 
-
-dataname = r'./user_done_participant/alluser_filtered.xlsx'
-data = xlrd.open_workbook(filename=dataname)
-sheet1 = data.sheet_by_index(1)
-user_id_set = sheet1.col_values(0)
-user_id_len = len(user_id_set)
-
-for stopnum in range(20000,user_id_len,5000):
-    startnum=stopnum-5000
+for stopnum in range(20000, user_id_len, 5000):
+    startnum = stopnum - 5000
     p_id = []
     p_time = []
     r_id = []
@@ -53,6 +41,7 @@ for stopnum in range(20000,user_id_len,5000):
     distance = []
     p_speed = []
     comment = []
+    print('正在创建' + str(startnum) + '至' + str(stopnum) + '的用户赛事参与信息！')
     for user_index in range(startnum, stopnum):
         user_id_test = user_id_set[user_index]
         allinfo = get_user_partinrace.getUInfo(user_id_test)
@@ -81,24 +70,68 @@ for stopnum in range(20000,user_id_len,5000):
     #     'id':id,
     #     'name':name
     # })
-    filename = './user_done_participant/test{}.xls'.format(str(stopnum))
+    filename = './user_done_participant/user_partake_info_test{}.xls'.format(str(stopnum))
     if os.path.exists(filename):
-        print('已存在文件名：'+'test{}'.format(str(stopnum)))
+        print('已存在文件名：' + 'test{}'.format(str(stopnum)))
     else:
-        data.to_excel(u'./user_done_participant/test{}.xls'.format(str(stopnum)), index=False, encoding='"utf_8_sig')
-        print('创建文件名：'+'test{}'.format(str(stopnum)))
+        data = pd.DataFrame({'p_id': p_id,
+                             'p_time': p_time,
+                             'r_id': r_id,
+                             'r_name': r_name,
+                             'd_time': d_time,
+                             'distance': distance,
+                             'p_speed': p_speed,
+                             'comment': comment
+                             })
+        data.to_excel(u'./user_done_participant/user_partake_info_test{}.xls'.format(str(stopnum)), index=False,
+                      encoding='"utf_8_sig')
+
+        print('已创建文件名：' + 'user_partake_info_test{}.xls'.format(str(stopnum)))
+        email_remind = email_attention.mail(stopnum)
+    time.sleep(10)
+
+p_id = []
+p_time = []
+r_id = []
+r_name = []
+d_time = []
+distance = []
+p_speed = []
+comment = []
+for user_index in range(60000, user_id_len):
+    user_id_test = user_id_set[user_index]
+    allinfo = get_user_partinrace.getUInfo(user_id_test)
+    allinfolen = [len(allinfo[0]),
+                  len(allinfo[1]),
+                  len(allinfo[2]),
+                  len(allinfo[3]),
+                  len(allinfo[4]),
+                  len(allinfo[5]),
+                  len(allinfo[6]),
+                  len(allinfo[7]),
+                  ]
+    p_id.extend(allinfo[0])
+    p_time.extend(allinfo[1])
+    r_id.extend(allinfo[2])
+    r_name.extend(allinfo[3])
+    d_time.extend(allinfo[4])
+    distance.extend(allinfo[5])
+    p_speed.extend(allinfo[6])
+    comment.extend(allinfo[7])
+    print('已载入第' + str(user_index + 1) + '个用户信息' + 'user_id为：' + user_id_set[user_index] + '\n')
+    time.sleep(1)
+
+data = pd.DataFrame({'p_id': p_id,
+                     'p_time': p_time,
+                     'r_id': r_id,
+                     'r_name': r_name,
+                     'd_time': d_time,
+                     'distance': distance,
+                     'p_speed': p_speed,
+                     'comment': comment
+                     })
+data.to_excel(u'./user_done_participant/user_partake_info_test63094.xls', index=False, encoding='"utf_8_sig')
+print('已创建文件名：user_partake_info_test63094.xls !')
+email_remind = email_attention.mail(63094)
+
 print('已全部创建完成！')
-
-
-
-data1 = pd.DataFrame({'p_id': p_id,
-                      'p_time': p_time,
-                      'r_id': r_id,
-                      'r_name': r_name,
-                      'd_time': d_time,
-                      'distance': distance,
-                      'p_speed': p_speed,
-                      'comment': comment
-                      })
-data1.to_excel(u'./user_done_participant/user_partake_info_test15000.xls', index=False, encoding='"utf_8_sig')
-print('测试信息写入完成！')
--- event_recommand/te3.py
+++ event_recommand/te3.py
@@ -1,31 +1,9 @@
-import os
-import time
-import smtplib
-from email.header import Header
-from email.mime.text import MIMEText
+import xlrd
 
-#设置smtplib所需的参数
-#下面的发件人，收件人是用于邮件传输的。
-smtpserver = 'smtp.yeah.net'
-username = 'youamg2@yeah.net'
-password='mikiyd299N'
-sender='youamg2@yeah.net'
-#receiver='XXX@126.com'
-#收件人可以为多个收件人
-receiver=['244300721@qq.com']
+dataname = r'./user_done_participant/alluser_filtered.xlsx'
+data = xlrd.open_workbook(filename=dataname)
+sheet1 = data.sheet_by_index(1)
+user_id_set = sheet1.col_values(0)
+user_id_len = len(user_id_set)
 
-message = MIMEText('Python 邮件发送测试to yeah邮箱', 'plain', 'utf-8')
-message['From'] = Header("菜鸟教程", 'utf-8')
-message['To'] = Header("测试", 'utf-8')
-
-subject = 'Python SMTP 邮件发送测试'
-message['Subject'] = Header(subject, 'utf-8')
-
-try:
-    smtpObj = smtplib.SMTP()
-    smtpObj.connect(smtpserver, 25)    # 25 为 SMTP 端口号
-    smtpObj.login(username,password)
-    smtpObj.sendmail(sender, receiver, message.as_string())
-    print("邮件发送成功")
-except smtplib.SMTPException:
-    print("Error: 无法发送邮件")
+print(user_id_set[user_id_len-1])
